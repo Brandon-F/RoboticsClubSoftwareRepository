c883b6b2ea9355cb653bd69c593868b1c5cd086b
Brandon-F <72361987+Brandon-F@users.noreply.github.com>
2021-02-27 13:50:19 -0800

    Pong 2

---
+++ GameFrame.java
@@ -0,0 +1,24 @@
+package Pong2;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class GameFrame extends Rectangle{
+
+    GamePanel panel;
+
+    GameFrame(){
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Pong Game");//this stops people from changing anything
+        this.setResizable(false);
+        this.setBackground(Color.BLACK);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack;
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        
+    }
+}
---
+++ PongGame.java
@@ -0,0 +1,15 @@
+package Pong2;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class PongGame{
+
+public void main(String[]args){
+
+    GameFrame frame = new GameFrame();
+}
+
+}
